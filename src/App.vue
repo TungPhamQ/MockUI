508e3d232c5535b34731daf038750855c5a4e24c
TungPhamQ <tungpham2109@gmail.com>
2022-06-24 17:47:05 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <NavBar />
+    </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+    name: 'App',
+    components: {
+        NavBar
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;700&display=swap');
+
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Noto Sans JP', sans-serif;
+}
+</style>
